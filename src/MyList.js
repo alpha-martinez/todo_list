425f79042a70f68ee38f962c1990bb36d481de7f
alpha-martinez <martinezalphaa@gmail.com>
2020-08-26 15:12:15 -0400

    finishing part 2

--- src/MyList.js
+++ src/MyList.js
@@ -28,43 +28,58 @@ import './App.css';
 import ListItem from './ListItem';
 
 class MyList extends Component {
-
-  constructor (props) {
-    super()
+  constructor(props) {
+    super();
     this.state = {
-      toDoItemArray: props.theList
-    }
+      toDoItemArray: props.theList,
+      newItem: "",
+    };
   }
 
-  clearList (e) {
-    //console.log('Clearing list!')
+  newItemChange(e) {
+    console.log("Changing Item!");
     this.setState({
-      toDoItemArray: []
-    })
+      newItem: e.target.value,
+    });
   }
 
-  render () {
-    let todoItems = this.state.toDoItemArray.map( (item, index) => (
+  addItem(e) {
+    e.preventDefault();
+    this.setState({
+      newArray: this.state.toDoItemArray.push(this.state.newItem),
+    });
+  }
+
+  clearList(e) {
+    //console.log("Clearing List!");
+    this.setState({
+      toDoItemArray: [],
+    });
+  }
+
+  render() {
+    let toDoItems = this.state.toDoItemArray.map((item, index) => (
       <ListItem doThis={item} key={ index } />
-    ))
+    ));
+
     return (
       <div>
-        <h1> Things I should stop procrastinating: </h1>
-        <ul>
-          {todoItems}
-        </ul>
+        <h1> Things I should stop procrastinating:</h1>
+        <ul>{toDoItems}</ul>
         <form>
-          <input type="text"
-          placeholder="Type an item here"
-          onChange={(e) => this.newItemChange(e)}
-          value={this.state.newItem}
+          <input
+            type="text"
+            placeholder="Type an item here"
+            onChange={(e) => this.newItemChange(e)}
+            value={this.state.newItem}
           />
           <button onClick={(e) => this.addItem(e)}>Add it!</button>
         </form>
-        <button onClick={(e) => this.clearList(e)}>Finish the list!</button>
+        <button onClick={(e) => this.clearList(e)}>Finished the list!</button>
       </div>
-    )
+    );
   }
 }
+
 
 export default MyList;
